7e468f774f7b032ed4b050710b30e9c8221f4af8
facu kaste <82892379+octantes@users.noreply.github.com>
2025-09-18 18:48:50 -0300

    Delete clase5 directory

--- clase5/Calculadora.java
+++
@@ -1,8 +0,0 @@
-package clase5;
-
-public class Calculadora { // recibe Cancion como parametro pero no lo guarda
-    public void calcular (Impuesto impuesto) {
-        System.out.println("Calculando impuesto de " + impuesto.getContribuyente().getNombre()
-                + " CUIL: " + impuesto.getContribuyente().getCuil() + ": $" + impuesto.getMonto());
-    }
-}
