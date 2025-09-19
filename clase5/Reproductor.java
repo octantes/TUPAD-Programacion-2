7f1400a365b8871537ea57d86b63840ca8606075
facu kaste <82892379+octantes@users.noreply.github.com>
2025-09-19 18:30:15 -0300

    Delete clase5 directory

--- clase5/Reproductor.java
+++
@@ -1,8 +0,0 @@
-package clase5;
-
-public class Reproductor { // recibe Cancion como parametro pero no lo guarda
-    public void reproducir(Cancion cancion) {
-        System.out.println("Reproduciendo: " + cancion.getTitulo() + " - De: " + cancion.getArtista().getNombre()
-                + " - Genero: " + cancion.getArtista().getGenero());
-    }
-}
